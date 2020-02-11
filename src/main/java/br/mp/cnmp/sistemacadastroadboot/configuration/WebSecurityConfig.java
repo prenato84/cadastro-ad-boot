2b0dc33fa25c420d21c0dda42390267731868d9b
Paulo Renato Alves de Melo Castro <PauloRenato@cnmp.ad>
2020-02-11 14:25:50 -0300

    Melhora página login

--- src/main/java/br/mp/cnmp/sistemacadastroadboot/configuration/WebSecurityConfig.java
+++ src/main/java/br/mp/cnmp/sistemacadastroadboot/configuration/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package br.mp.cnmp.sistemacadastroadboot.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -40,4 +41,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		return new InMemoryUserDetailsManager(user);
 	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	    web
+	       .ignoring()
+	       .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+    }
 }
