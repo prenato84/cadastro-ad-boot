c3ba52d87c20ecce9bda5e9fa531267720b94246
Paulo Renato Alves de Melo Castro <PauloRenato@cnmp.ad>
2020-02-12 13:31:02 -0300

    Corrige errors com Webjars

--- src/main/java/br/mp/cnmp/sistemacadastroadboot/configuration/WebSecurityConfig.java
+++ src/main/java/br/mp/cnmp/sistemacadastroadboot/configuration/WebSecurityConfig.java
@@ -43,7 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/", "/login").permitAll()
+				.antMatchers("/", "/login", "/webjars/**").permitAll()
 				//.antMatchers("/hello/**").access("hasRole('ADM_DOMINIO') and hasRole('USER')")
 				.antMatchers("/hello/**").access("hasRole('ADM_DOMINIO')")
 				.anyRequest()
