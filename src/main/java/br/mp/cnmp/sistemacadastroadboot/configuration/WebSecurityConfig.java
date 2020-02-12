19a15bbb5c2b4c28967197ad823c0ed92cbfdfb8
Paulo Renato Alves de Melo Castro <PauloRenato@cnmp.ad>
2020-02-12 09:14:59 -0300

    Aunteticação simples pelo LDAP

--- src/main/java/br/mp/cnmp/sistemacadastroadboot/configuration/WebSecurityConfig.java
+++ src/main/java/br/mp/cnmp/sistemacadastroadboot/configuration/WebSecurityConfig.java
@@ -1,47 +1,73 @@
 package br.mp.cnmp.sistemacadastroadboot.configuration;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Value("${ldap.url}")
+	private String ldapUrl;
+
+	@Value("${ldap.port}")
+	private String ldapPort;
+
+	@Value("${ldap.user.search.base}")
+	private String ldapUserSearchBase;
+
+	@Value("${ldap.user.search.filter}")
+	private String ldapUserSearchFilter;
+
+	@Value("${ldap.group.search.base}")
+	private String ldapGroupSearchBase;
+
+	@Value("${ldap.group.search.filter}")
+	private String ldapGroupSearchFilter;
+
+	@Value("${ldap.manager.dn}")
+	private String ldapManagerDn;
+
+	@Value("${ldap.password.dn}")
+	private String ldapPasswordDn;
+
+	@Value("${ldap.enabled}")
+	private String ldapEnabled;
+
+	// Configura as autorizações/permissões
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/", "/home").permitAll()
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.loginPage("/login")
-				.permitAll()
-				.and()
-			.logout()
-				.permitAll();
+		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
+				.loginPage("/login").permitAll().and().logout().permitAll();
 	}
 
-	@Bean
+	// Configura a autenticação pelo LDAP ou em memória para testes
 	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("usuario")
-				.password("senha")
-				.roles("USER")
-				.build();
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		return new InMemoryUserDetailsManager(user);
+		if (Boolean.parseBoolean(ldapEnabled)) {
+			auth.ldapAuthentication().userSearchBase(ldapUserSearchBase)
+					.userSearchFilter("(" + ldapUserSearchFilter + ")").groupSearchBase(ldapGroupSearchBase)
+					.groupSearchFilter(ldapGroupSearchFilter).contextSource().url(ldapUrl)
+					.port(Integer.parseInt(ldapPort))
+					// .url("ldaps://hurley1.cnmp.ad/DC=cnmp,DC=ad")
+					// .port(639)
+					.managerDn(ldapManagerDn).managerPassword(ldapPasswordDn);
+		} else {
+			auth
+				.inMemoryAuthentication()
+					.withUser("user").password("{noop}user").roles("USER")
+					.and()
+					.withUser("admin").password("{noop}admin").roles("ADMIN");
+		}
 	}
 
+	// Exclui os diretórios abaixo da camada de segurança do Spring Security
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 	    web
