e35ea05f077d88abfb5a040c05513ab28468c5ec
Paulo Renato Alves de Melo Castro <PauloRenato@cnmp.ad>
2020-02-13 14:24:44 -0300

    Remove arquivos antigos do front

--- src/main/java/br/mp/cnmp/sistemacadastroadboot/configuration/MvcConfig.java
+++ src/main/java/br/mp/cnmp/sistemacadastroadboot/configuration/MvcConfig.java
@@ -8,9 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("login2");
+		registry.addViewController("/").setViewName("login");
 		registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login2");
+        registry.addViewController("/login").setViewName("login");
 	}
 
 }
