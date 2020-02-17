aa8d3109d46440c81dc57b7cdb5b8764a612b865
Paulo Renato Alves de Melo Castro <PauloRenato@cnmp.ad>
2020-02-17 17:00:32 -0300

    Adiciona Unbounid

--- src/main/java/br/mp/cnmp/sistemacadastroadboot/configuration/WebSecurityConfig.java
+++ src/main/java/br/mp/cnmp/sistemacadastroadboot/configuration/WebSecurityConfig.java
@@ -1,13 +1,18 @@
 package br.mp.cnmp.sistemacadastroadboot.configuration;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 
 /*
 ** Classe responsável pela configuração, autenticação, autorização e permissão de acesso do Spring Security e do Spring Security LDAP
@@ -31,6 +36,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Value("${ldap.enabled}")
 	private String ldapEnabled;
+
+	@Autowired
+	Environment env;
 
 	// Configura as autorizações/permissões de segurança do Spring Security
 	@Override
@@ -73,8 +81,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			auth.ldapAuthentication()
 					.userSearchBase("ou=Terceirizados,ou=eDirectory,DC=cnmp,DC=ad")
 					.userSearchFilter("(sAMAccountName={0})")
-					/* base de busca dos grupos de permissões principais (usuário tem que ser membro de um dos grupos dessa OU, que posteriormente é
-					filtrado na página HTML por meio do Thymeleaf Sec) */
+					// base de busca dos grupos de permissões principais (usuário tem que ser membro de um dos grupos dessa OU, que posteriormente é
+					// filtrado na página HTML por meio do Thymeleaf Sec)
 					.groupSearchBase("ou=STI,ou=eDirectory,DC=cnmp,DC=ad")
 					.groupSearchFilter("member={0}")
 					.contextSource()
@@ -90,6 +98,43 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 					.withUser("admin").password("{noop}admin").roles("GP_ZEN_ADMIN");
 		}
 	}
+
+	// Configura a autenticação pelo LDAP (Spring Security LDAP) no srevidor de teste UnboundID
+	/* @Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		// usuários da OU=STI
+		auth.ldapAuthentication()
+				// dn: CN=Paulo Renato Alves de Melo Castro,OU=STI,OU=eDirectory,DC=cnmp,DC=ad
+				.userSearchFilter("sAMAccountName={0}")
+				.groupSearchBase("ou=STI,ou=eDirectory,DC=cnmp,DC=ad")
+				.groupSearchFilter("member={0}")
+				.contextSource()
+					.url("ldap://localhost:389")
+				.managerDn("cn=ldapUserApp,ou=STI,ou=eDirectory,dc=cnmp,DC=ad")
+				.managerPassword("Mudarsenh@");
+		// usuários da OU=Terceirizados
+		auth.ldapAuthentication()
+				.userSearchBase("ou=Terceirizados,ou=eDirectory,DC=cnmp,DC=ad")
+				.userSearchFilter("(sAMAccountName={0})")
+				// base de busca dos grupos de permissões principais (usuário tem que ser membro de um dos grupos dessa OU, que posteriormente é
+				// filtrado na página HTML por meio do Thymeleaf Sec)
+				.groupSearchBase("ou=STI,ou=eDirectory,DC=cnmp,DC=ad")
+				.groupSearchFilter("member={0}")
+				.contextSource()
+				.url("ldap://localhost:389")
+				.managerDn("cn=ldapUserApp,ou=STI,ou=eDirectory,dc=cnmp,DC=ad")
+				.managerPassword("Mudarsenh@");
+	} */
+
+	/*
+	** Autenticação pelo servidor embedded LDAP de TESTE (UnboundID)  
+	 */
+	/* @Bean
+	public DefaultSpringSecurityContextSource contextSource() {
+		return  new DefaultSpringSecurityContextSource(Arrays.asList("ldap://localhost:" 
+					+ env.getRequiredProperty("spring.ldap.embedded.port") + "/"), env.getRequiredProperty("spring.ldap.embedded.base-dn"));
+	} */
 
 	// Exclui os diretórios abaixo da camada de segurança do Spring Security
 	/* @Override
