3030a1a3971b12955723fe470eb7ccd0beb68942
Paulo Renato Alves de Melo Castro <prenato84@gmail.com>
2020-02-18 21:00:38 -0300

    Corrigi LDAP memória , AD e LDIF

--- src/main/java/br/mp/cnmp/sistemacadastroadboot/configuration/WebSecurityConfig.java
+++ src/main/java/br/mp/cnmp/sistemacadastroadboot/configuration/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
 
@@ -35,14 +36,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Value("${ldap.password.dn}")
 	private String ldapPasswordDn;
 
-	@Value("${ldap.enabled}")
-	private String ldapEnabled;
-
 	@Value("${ldap.embedded}")
 	private String ldapEmbedded;
-
-	@Autowired
-	Environment env;
 
 	// Configura as autorizações/permissões de segurança do Spring Security
 	@Override
@@ -65,91 +60,82 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.permitAll();
 	}
 
-	// Configura a autenticação pelo LDAP (Spring Security LDAP) ou em memória para testes
+	// Configura a autenticação pelo LDAP (Spring Security LDAP)
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		if (Boolean.parseBoolean(ldapEnabled)) {
-			if (!Boolean.parseBoolean(ldapEmbedded)) {
-				ActiveDirectoryLdapAuthenticationProvider adProvider = 
-                    new ActiveDirectoryLdapAuthenticationProvider("cnmp.ad","ldap://hurley1.cnmp.ad:389");
-				adProvider.setConvertSubErrorCodesToExceptions(true);
-				adProvider.setUseAuthenticationRequestCredentials(true);
-		
-				// set pattern if it exists
-				// The following example would authenticate a user if they were a member
-				// of the ServiceAccounts group
-				// (&(objectClass=user)(userPrincipalName={0})
-				//   (memberof=CN=ServiceAccounts,OU=alfresco,DC=mycompany,DC=com))
-				adProvider.setSearchFilter("(&(objectClass=user)(userPrincipalName={0})(memberOf=CN=GP_ZEN_ADMIN,OU=STI,OU=eDirectory,DC=cnmp,DC=ad))");
-				
-				auth.authenticationProvider(adProvider);
-				
-				// don't erase credentials if you plan to get them later
-				// (e.g using them for another web service call)
-				auth.eraseCredentials(false);
-				/* 
-				// usuários da OU=STI
-				auth.ldapAuthentication()
-						.userSearchBase("ou=STI,ou=eDirectory,DC=cnmp,DC=ad")
-						.userSearchFilter("(sAMAccountName={0})")
-						.groupSearchBase("ou=STI,ou=eDirectory,DC=cnmp,DC=ad")
-						.groupSearchFilter("member={0}")
-						.contextSource()
-						.url(ldapUrl)
-						.port(Integer.parseInt(ldapPort))
-						.managerDn(ldapManagerDn)
-						.managerPassword(ldapPasswordDn);
-				// usuários da OU=Terceirizados
-				auth.ldapAuthentication()
-						.userSearchBase("ou=Terceirizados,ou=eDirectory,DC=cnmp,DC=ad")
-						.userSearchFilter("(sAMAccountName={0})")
-						// base de busca dos grupos de permissões principais (usuário tem que ser membro de um dos grupos dessa OU, que posteriormente é
-						// filtrado na página HTML por meio do Thymeleaf Sec)
-						.groupSearchBase("ou=STI,ou=eDirectory,DC=cnmp,DC=ad")
-						.groupSearchFilter("member={0}")
-						.contextSource()
-						.url(ldapUrl)
-						.port(Integer.parseInt(ldapPort))
-						.managerDn(ldapManagerDn)
-						.managerPassword(ldapPasswordDn);
-			// ldapEmbedded = true => servidor LDAP UnboundID */
-			} else {
-				auth.ldapAuthentication()
-					.userSearchFilter("sAMAccountName={0}")
+		// se ldapEmbedded = false, a autenticação ocorrerá no servidor AD hurley1.cnmp.ad
+		if (!Boolean.parseBoolean(ldapEmbedded)) {
+			
+			ActiveDirectoryLdapAuthenticationProvider adProvider = 
+				new ActiveDirectoryLdapAuthenticationProvider("cnmp.ad","ldap://hurley1.cnmp.ad:389");
+			adProvider.setConvertSubErrorCodesToExceptions(true);
+			adProvider.setUseAuthenticationRequestCredentials(true);
+	
+			// set pattern if it exists
+			// The following example would authenticate a user if they were a member
+			// of the ServiceAccounts group
+			// (&(objectClass=user)(userPrincipalName={0})
+			//   (memberof=CN=ServiceAccounts,OU=alfresco,DC=mycompany,DC=com))
+			adProvider.setSearchFilter("(&(objectClass=user)(userPrincipalName={0})(memberOf=CN=GP_ZEN_ADMIN,OU=STI,OU=eDirectory,DC=cnmp,DC=ad))");
+			
+			auth.authenticationProvider(adProvider);
+			
+			// don't erase credentials if you plan to get them later
+			// (e.g using them for another web service call)
+			auth.eraseCredentials(false);
+			
+			/* 
+			// usuários da OU=STI
+			auth.ldapAuthentication()
+					.userSearchBase("ou=STI,ou=eDirectory,DC=cnmp,DC=ad")
+					.userSearchFilter("(sAMAccountName={0})")
 					.groupSearchBase("ou=STI,ou=eDirectory,DC=cnmp,DC=ad")
 					.groupSearchFilter("member={0}")
 					.contextSource()
-						.url("ldap://localhost:389")
+					.url(ldapUrl)
+					.port(Integer.parseInt(ldapPort))
 					.managerDn(ldapManagerDn)
 					.managerPassword(ldapPasswordDn);
-				// usuários da OU=Terceirizados
-				auth.ldapAuthentication()
-						.userSearchBase("ou=Terceirizados,ou=eDirectory,DC=cnmp,DC=ad")
-						.userSearchFilter("(sAMAccountName={0})")
-						// base de busca dos grupos de permissões principais (usuário tem que ser membro de um dos grupos dessa OU, que posteriormente é
-						// filtrado na página HTML por meio do Thymeleaf Sec)
-						.groupSearchBase("ou=STI,ou=eDirectory,DC=cnmp,DC=ad")
-						.groupSearchFilter("member={0}")
-						.contextSource()
-							.url("ldap://localhost:389")
-						.managerDn(ldapManagerDn)
-						.managerPassword(ldapPasswordDn);
-			}
-		// ldapEnabled = false => autenticação em memória
+			// usuários da OU=Terceirizados
+			auth.ldapAuthentication()
+					.userSearchBase("ou=Terceirizados,ou=eDirectory,DC=cnmp,DC=ad")
+					.userSearchFilter("(sAMAccountName={0})")
+					// base de busca dos grupos de permissões principais (usuário tem que ser membro de um dos grupos dessa OU, que posteriormente é
+					// filtrado na página HTML por meio do Thymeleaf Sec)
+					.groupSearchBase("ou=STI,ou=eDirectory,DC=cnmp,DC=ad")
+					.groupSearchFilter("member={0}")
+					.contextSource()
+					.url(ldapUrl)
+					.port(Integer.parseInt(ldapPort))
+					.managerDn(ldapManagerDn)
+					.managerPassword(ldapPasswordDn);
+
+		// ldapEmbedded = true => servidor de teste LDAP UnboundID */
 		} else {
-			// usuários em memória (alterar a propriedade "ldap.enabled= false")
-			auth.inMemoryAuthentication()
-					.withUser("user").password("{noop}user").roles("USER")
+			
+			// usuários com senha setadas manualmente no CNMP_AD.ldif (https://bcrypt-generator.com/):
+			// testesti, [senha], ou=STI, memberOf=não tem GP_ZEN_ADMIN
+			// tercservsat, [senha], ou=Terceirizados, memberOf=GP_ZEN_ADMIN
+			// ldapUserApp, [senha], ou=STI, memberOf=GP_ZEN_ADMIN
+			auth.ldapAuthentication()
+				.userSearchFilter("sAMAccountName={0}")
+				.userSearchBase("ou=eDirectory,DC=cnmp,DC=ad")
+				.groupSearchBase("ou=STI,ou=eDirectory,DC=cnmp,DC=ad")
+				.groupSearchFilter("member={0}")
+				.contextSource()
+					.url("ldap://localhost:389")
 					.and()
-					.withUser("admin").password("{noop}admin").roles("GP_ZEN_ADMIN");
+					.passwordCompare()
+						.passwordEncoder(new BCryptPasswordEncoder())
+						.passwordAttribute("userPassword");
 		}
 	}
 
 	/*
 	** Autenticação pelo servidor embedded LDAP de TESTE (UnboundID)  
 	 */
-	/* @Bean
+	/*@Bean
 	public DefaultSpringSecurityContextSource contextSource() {
 		if (Boolean.parseBoolean(ldapEmbedded)) {
 			return  new DefaultSpringSecurityContextSource(Arrays.asList("ldap://localhost:" 
@@ -158,7 +144,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			return  new DefaultSpringSecurityContextSource(Arrays.asList("ldap://hurley1.cnmp.ad:" 
 						+ "389" + "/"), "DC=cnmp,DC=ad");
 		}
-	} */
+	}*/
 
 	// Exclui os diretórios abaixo da camada de segurança do Spring Security
 	/* @Override
--- src/main/java/br/mp/cnmp/sistemacadastroadboot/controllers/MainController.java
+++ src/main/java/br/mp/cnmp/sistemacadastroadboot/controllers/MainController.java
@@ -1,7 +1,6 @@
 package br.mp.cnmp.sistemacadastroadboot.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
